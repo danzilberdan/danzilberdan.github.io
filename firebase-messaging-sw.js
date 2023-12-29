b01d092f126ea519b274e43207685c1e4be076eb
dan <dan.zilberman@brightmachines.com>
2023-12-29 08:46:06 +0200

    Working FCM push notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPq24_mICDA2ZKgDwG6nxDWtcFkrzIlo4",
+  authDomain: "blog-ba65c.firebaseapp.com",
+  projectId: "blog-ba65c",
+  storageBucket: "blog-ba65c.appspot.com",
+  messagingSenderId: "870065325045",
+  appId: "1:870065325045:web:8988fc00ae630fc9c056bc",
+  measurementId: "G-PV6ZD4J667"
+};
+
+// // Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = firebase.messaging()
